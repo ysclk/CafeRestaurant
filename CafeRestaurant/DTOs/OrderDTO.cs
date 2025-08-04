63365f8e97e9d658903f012374958e7b9144da25
ysclk <34403708+ysclk@users.noreply.github.com>
2025-08-05 00:08:13 +0300

    Update OrderDTO.cs

--- CafeRestaurant/DTOs/OrderDTO.cs
+++ CafeRestaurant/DTOs/OrderDTO.cs
@@ -10,24 +10,20 @@ namespace CafeRestaurant.DTOs
     /// Represents a data transfer object for order details, 
     /// including product, customer, category, and payment information.
     /// </summary>
-    internal class OrderDTO
+    public class OrderDTO
     {
+        public string CUSTOMERNAME { get; set; }
+        public int CUSTOMERID { get; set; }
         public int PRODUCTID { get; set; }
         public string PRODUCTNAME { get; set; }
-        public int QUANTITY { get; set; }
+        public int STOCK { get; set; }
         public decimal PRODUCTPRICE { get; set; }
+   //     public string ORDERPAID { get; set; }
+        public string ORDERTYPE { get; set; }
         public decimal SUBTOTAL { get; set; }
         public string ORDERNOTE { get; set; }
-        public int CUSTOMERID { get; set; }
-        public string CUSTOMERNAME { get; set; }
-        public string ISPAID { get; set; }
-        public string ORDERTYPE { get; set; }
-        public int CATEGORYID {  get; set; }
-        public string CATEGORYNAME {  get; set; }        
-   
-       
-      
-      
-
+        public int QUANTITY { get; set; }
+        public int ORDERID { get; set; }
+        public int ORDERDETAILID { get; set; }
     }
 }

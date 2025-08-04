ec4a18bf773db9831fe8798f8263a893961102ab
ysclk <34403708+ysclk@users.noreply.github.com>
2025-08-05 00:10:15 +0300

    Update PasswordChangeForm.Designer.cs

--- CafeRestaurant/Forms/PasswordChangeForm.Designer.cs
+++ CafeRestaurant/Forms/PasswordChangeForm.Designer.cs
@@ -97,7 +97,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(33, 25);
+            this.label1.Location = new System.Drawing.Point(22, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(348, 41);
             this.label1.TabIndex = 0;

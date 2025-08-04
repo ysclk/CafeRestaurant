5c3c3445076df5cf21c48e3923ce48729fba0e20
ysclk <34403708+ysclk@users.noreply.github.com>
2025-08-05 00:13:07 +0300

    Create StockService.cs

---
+++ CafeRestaurant/Services/StockService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace CafeRestaurant.Services
+{
+    internal class StockService: BaseService<PRODUCTSTOCK>
+    {
+       
+        public PRODUCTSTOCK ProductStockByProductid(int productId)
+        {
+            return db.PRODUCTSTOCK.FirstOrDefault(t => t.PRODUCT_ID == productId);
+        }
+
+        public virtual void UpdateStock(int productId) 
+           {
+            var stock = db.PRODUCTSTOCK.FirstOrDefault(x => x.PRODUCT_ID == productId);
+            if (stock != null)
+            {
+               
+                db.SaveChanges();
+            }
+        }
+
+        public int GetStock(int productId)
+        {
+            return (from i in db.PRODUCTSTOCK
+            where i.PRODUCT_ID == productId
+            select i.STOCK).FirstOrDefault() ?? 0;
+        }
+    }
+}

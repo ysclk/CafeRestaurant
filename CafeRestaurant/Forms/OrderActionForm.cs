43eea39012a97f8d99279b191a1adcc83c17b917
ysclk <34403708+ysclk@users.noreply.github.com>
2025-08-05 00:09:43 +0300

    Create OrderActionForm.cs

---
+++ CafeRestaurant/Forms/OrderActionForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CafeRestaurant.Forms
+{
+    public partial class OrderActionForm : Form
+    {
+        public OrderActionForm()
+        {
+            InitializeComponent();
+        }
+        public event Action OnCancelClicked;
+        public event Action OnDeliveredClicked;
+        private void OrderActionForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelivered_Click(object sender, EventArgs e)
+        {
+            OnDeliveredClicked?.Invoke();
+            this.Close();
+        }
+
+        private void btnOrderCancel_Click(object sender, EventArgs e)
+        {
+            OnCancelClicked?.Invoke();
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}

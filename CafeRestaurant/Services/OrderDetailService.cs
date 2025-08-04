482d8b1609c8e8bca4b77401b756a5cce15a006d
ysclk <34403708+ysclk@users.noreply.github.com>
2025-08-05 00:12:53 +0300

    Update OrderDetailService.cs

--- CafeRestaurant/Services/OrderDetailService.cs
+++ CafeRestaurant/Services/OrderDetailService.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CafeRestaurant.Services
 {
     internal class OrderDetailService:BaseService<ORDERDETAIL>
     {
+
+        public void MarkOrderAsDelivered(int orderDetailId)
+        {
+           
+                var order = db.ORDERDETAIL.FirstOrDefault(o => o.ORDERDETAILID == orderDetailId);
+
+                if (order == null)
+                    throw new Exception("Null");
+
+                if (order != null) order.ORDERSTATUS = 2;
+
+                db.SaveChanges();
+            
+        }
+
+        public void CancelOrder(int orderDetailId)
+        {
+          
+                var order = db.ORDERDETAIL.FirstOrDefault(o => o.ORDERDETAILID == orderDetailId);
+
+                if (order == null)
+                    throw new Exception("Null");
+
+                if (order != null) order.ORDERSTATUS = 3;
+                db.SaveChanges();
+                MessageBox.Show("Sipariş iptal edildi.");
+            
+        }
     }
 }

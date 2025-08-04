e375469244b7f408967a3d1ad24d518c97aab19c
ysclk <34403708+ysclk@users.noreply.github.com>
2025-08-05 00:10:19 +0300

    Update ResetPasswordForm.cs

--- CafeRestaurant/Forms/ResetPasswordForm.cs
+++ CafeRestaurant/Forms/ResetPasswordForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CafeRestaurant.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,24 @@ namespace CafeRestaurant.Forms
         {
             InitializeComponent();
         }
+        private AuthService authService = new AuthService();
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            int userId = authService.userId(txbUsermail.Text);
+            if (userId == 0)
+            {
+                MessageBox.Show("Not User");
+            }
+            PasswordChangeForm passForm = new PasswordChangeForm(userId,txbUsermail.Text);            
+            this.Close();
+            passForm.Show();
+
+        }
+       
     }
 }
